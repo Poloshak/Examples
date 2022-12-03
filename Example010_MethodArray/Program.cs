0421a2b33da7f6b3b22b57dfc3c81f862fc60c5b
Poloshak <e.l.fomenko@yandex.ru>
2022-12-03 18:19:44 +0300

    Add Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {15,26,34,38,57,699,57,38};
+
+int n = array.Length;
+int find = 38;
+
+int index = 0;
+
+while (index<n)
+{
+  if(array[index] == find)
+  {
+   Console.WriteLine(index);  
+   break;
+  }
+  //index = index+1;
+  index++; 
+}
+
